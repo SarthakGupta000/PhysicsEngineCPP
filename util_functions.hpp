6c8934866d9f194061f4c5987f4b03eb7240e037
Sarthak Gupta <sarthaksrws@icloud.com>
2026-05-11 21:12:07 +0530

    Modifying and extending functions

--- util_functions.hpp
+++ util_functions.hpp
@@ -1,15 +1,22 @@
 #include <vector>
-#include <string>
+#include <cmath>
 
-std::vector<float> updateVelocity(std::vector<float> vel, std::vector<float> acc, float time) { // for projectile motion only
+std::vector<float> updateVelocity(std::vector<float> vel, std::vector<float> acc, float time) { // not for collisions
     std::vector<float> v = vel;
     v[1] += acc[1] * time;
     return v;
 }
 
-std::vector<float> updatePosition(std::vector<float> pos, std::vector<float> vel) {
+std::vector<float> updatePosition(std::vector<float> pos, std::vector<float> vel, float time) {
     std::vector<float> p = pos;
-    p[0] = pos[0] + vel[0];
-    p[1] = pos[1] + vel[1];
+    p[0] += vel[0] * time;
+    p[1] += vel[1] * time;
     return p;
+}
+
+std::vector<float> getNormalBetweenCircles(std::vector<float> pos1, float rad1, std::vector<float> pos2, float rad2) { // use only when collision is there
+    float num = (float) pow((pos1[0] - pos2[0]), 2) + (float) pow((pos1[1] - pos2[1]), 2);
+    float dist = (float) pow(num, 0.5);
+    std::vector<float> normal = {(pos1[0] - pos2[0]) / dist, (pos1[1] - pos2[1]) / dist};
+    return normal;
 }
