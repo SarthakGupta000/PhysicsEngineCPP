ea635a9a51ca65813ca020590590039885086b83
Sarthak Gupta <sarthaksrws@icloud.com>
2026-05-12 18:00:46 +0530

    Changing file structure and tweaking code

--- util.hpp
+++ util.hpp
@@ -1,6 +1,6 @@
 #include <vector>
+#include <cmath>
 #include <string>
-#include "util_functions.hpp"
 
 class World {
     public:
@@ -30,12 +30,32 @@ class RigidObject {
 class Circle : public RigidObject {
     float radius;
 
-    Circle(float r, float m, std::vector<float> vel, std::vector<float> acc, std::vector<float> pos) : RigidObject(m, vel, acc, pos) {}
+    Circle(float r, float m, std::vector<float> vel, std::vector<float> acc, std::vector<float> pos) : RigidObject(m, vel, acc, pos), radius(r) {}
 };
 
 class Wall : public RigidObject {
     float x;
     float y;
 
-    Wall(float width, float height, float m, std::vector<float> vel, std::vector<float> acc, std::vector<float> pos) : RigidObject(m, vel, acc, pos) {}
+    Wall(float width, float height, std::vector<float> vel, std::vector<float> acc, std::vector<float> pos) : RigidObject(0, vel, acc, pos), x(width), y(height) {}
 };
+
+std::vector<float> updateVelocity(Circle circle, float time) { // not for collisions
+    std::vector<float> v = circle.velocity;
+    v[0] += circle.acceleration[0] * time;
+    v[1] += circle.acceleration[1] * time;
+    return v;
+}
+
+std::vector<float> updatePosition(Circle circle, float time) {
+    std::vector<float> p = circle.position;
+    p[0] += circle.velocity[0] * time;
+    p[1] += circle.velocity[1] * time;
+    return p;
+}
+
+float getDistance(std::vector<float> point1, std::vector<float> point2) {
+    float num = (float) pow((point1[0] - point2[0]), 2) + (float) pow((point1[1] - point2[1]), 2);
+    float dist = (float) pow(num, 0.5);
+    return dist;
+}
