c1975e2745598fdf79a005a88403a3f490583e39
Sarthak Gupta <sarthaksrws@icloud.com>
2026-05-14 16:59:37 +0530

    Completing the core program

--- util.hpp
+++ util.hpp
@@ -1,18 +1,8 @@
 #include <vector>
 #include <cmath>
 #include <string>
-
-class World {
-    public:
-    int y; // height
-    int x; // width
-    std::vector<float> g; // acceleration by gravity
-    std::string window_name;
-
-    World(int height, int width, std::vector<float> gravity, std::string name) : y(height), x(width), window_name(name), g(gravity) {}
-
-    void gameLoop(int fps, RigidObject object); // main loop for code
-};
+#include <chrono>
+#include <thread>
 
 class RigidObject {
     public:
@@ -39,14 +29,14 @@ class Wall : public RigidObject {
     Wall(float width, float height, std::vector<float> vel, std::vector<float> acc, std::vector<float> pos) : RigidObject(0, vel, acc, pos), x(width), y(height) {}
 };
 
-std::vector<float> updateVelocity(Circle circle, float time) { // not for collisions
+std::vector<float> updateVelocity(Circle& circle, float time) { // not for collisions
     std::vector<float> v = circle.velocity;
     v[0] += circle.acceleration[0] * time;
     v[1] += circle.acceleration[1] * time;
     return v;
 }
 
-std::vector<float> updatePosition(Circle circle, float time) {
+std::vector<float> updatePosition(Circle& circle, float time) {
     std::vector<float> p = circle.position;
     p[0] += circle.velocity[0] * time;
     p[1] += circle.velocity[1] * time;
@@ -55,14 +45,14 @@ std::vector<float> updatePosition(Circle circle, float time) {
 
 float getDistance(std::vector<float> point1, std::vector<float> point2) {
     float num = (float) pow((point1[0] - point2[0]), 2) + (float) pow((point1[1] - point2[1]), 2);
-    float dist = (float) pow(num, 0.5);
+    float dist = (float) sqrt(num);
     return dist;
 }
 
 // returns
 // posx posy
 // velx vely
-std::vector<std::vector<float>> handleWallCollision(Wall wall, Circle circle, float time, float restitution, float friction) { // res and fric between 0 and 1
+std::vector<std::vector<float>> handleWallCollision(Wall& wall, Circle& circle, float time, float restitution, float friction) { // res and fric between 0 and 1
     // cases not to handle collision for
     if (circle.position[1] < wall.position[1] - (wall.y / 2) - circle.radius || circle.position[1] > wall.position[1] + (wall.y / 2) + circle.radius) {
         std::vector<std::vector<float>> toreturn = {circle.position, circle.velocity};
@@ -87,4 +77,50 @@ std::vector<std::vector<float>> handleWallCollision(Wall wall, Circle circle, fl
     posclone[0] += velclone[0] * time;
     std::vector<std::vector<float>> toreturn = {posclone, velclone};
     return toreturn;
+}
+
+class World {
+    public:
+    int y; // height
+    int x; // width
+    std::vector<float> g; // acceleration by gravity
+    std::string window_name;
+
+    World(int height, int width, std::vector<float> gravity, std::string name) : y(height), x(width), window_name(name), g(gravity) {}
+
+    void gameLoop(float g, float friction, float restitution, int fps, Wall *wallObjects, int numOfWallObjects, Circle *circleObjects, int numOfCircleObjects); // main loop for code
+};
+
+void World::gameLoop(float g, float friction, float restitution, int fps, Wall *wallObjects, int numOfWallObjects, Circle *circleObjects, int numOfCircleObjects) { // g must be positive
+    double dt = (double) 1 / fps;
+    for (int x = 0; x < numOfCircleObjects; x++) {
+        circleObjects[x].acceleration = {0, (-1 * g)};
+    }
+    auto previousTime = std::chrono::steady_clock::now();
+    auto frameTime = std::chrono::steady_clock::now() - previousTime; // dummy
+    auto accumulator = previousTime - previousTime; // dummy
+    while (1) { // main loop for everything
+        auto currentTime = std::chrono::steady_clock::now();
+        frameTime = currentTime - previousTime;
+        previousTime = currentTime;
+        accumulator += frameTime;
+        while ((std::chrono::duration<double>(accumulator).count()) >= dt) {
+            // updating vel and pos
+            for (int i = 0; i < numOfCircleObjects; i++) {
+                circleObjects[i].velocity = updateVelocity(circleObjects[i], dt);
+                circleObjects[i].position = updatePosition(circleObjects[i], dt);
+            }
+            accumulator -= std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<double>(dt));
+            // handling collisions
+            std::vector<std::vector<float>> mat {{}, {}};
+            for (int i = 0; i < numOfCircleObjects; i++) {
+                for (int j = 0; j < numOfWallObjects; j++) {
+                    mat = handleWallCollision(wallObjects[j], circleObjects[i], dt, restitution, friction);
+                    circleObjects[i].velocity = mat[1];
+                    circleObjects[i].position = mat[0];
+                }
+            }
+        }
+        // rendering happens here
+    }
 }
